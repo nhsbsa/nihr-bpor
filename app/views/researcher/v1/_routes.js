037cd24a8b938318f6ff70f716e1ba565e4e74d3
Stephen Hill <stephen.hill@nhsbsa.nhs.uk>
2025-12-03 19:19:44 +0000

    Upgraded to v10.2.1 and added some researcher account screens

--- app/views/researcher/v1/_routes.js
+++ app/views/researcher/v1/_routes.js
@@ -7,12 +7,121 @@ const express = require('express');
 const router = express.Router();
 
 // ********************
+// Account Registration
+// ********************
+
+router.post('/index', function (req, res) {
+
+    res.redirect('create-an-account');
+
+});
+
+router.post('/create-an-account', function (req, res) {
+
+    var emailAddress = req.session.data['emailAddress'];
+    var password = req.session.data['password'];
+
+    if (emailAddress && password) {
+
+        res.redirect('verify-your-email');
+
+    } else {
+
+        res.redirect('create-an-account');
+
+    }
+
+});
+
+router.post('/tell-us-about-yourself', function (req, res) {
+
+    var title = req.session.data['title'];
+    var firstName = req.session.data['firstName'];
+    var lastName = req.session.data['lastName'];
+    var emailAddress = req.session.data['emailAddress'];
+    var organisation = req.session.data['organisation'];
+    var termsConditions = req.session.data['termsConditions'];
+    var privacyNotice = req.session.data['privacyNotice'];
+
+    if (title && firstName && lastName && emailAddress && organisation) {
+
+        res.redirect('sign-up-answers');
+
+    } else {
+
+        res.redirect('tell-us-about-yourself');
+
+    }
+
+});
+
+router.post('/sign-up-answers', function (req, res) {
+
+    res.redirect('registration-submitted');
+
+});
+
+// ********************
+// Account Login
+// ********************
+
+router.post('/researcher-login', function (req, res) {
+
+    var emailAddress = req.session.data['emailAddress'];
+    var password = req.session.data['password'];
+
+    if (emailAddress && password) {
+
+        res.redirect('home');
+
+    } else {
+
+        res.redirect('researcher-login');
+
+    }
+
+});
+
+router.post('/forgot-password', function (req, res) {
+
+    var emailAddress = req.session.data['emailAddress'];
+
+    if (emailAddress) {
+
+        res.redirect('check-your-email');
+
+    } else {
+
+        res.redirect('forgot-password');
+
+    }
+
+});
+
+router.post('/reset-your-password', function (req, res) {
+
+    var password1 = req.session.data['password1'];
+    var password2 = req.session.data['password2'];
+
+if (password1 && password2 && password1 === password2) {
+    
+    res.redirect('password-changed');
+
+} else {
+
+    res.redirect('reset-your-password');
+
+}
+
+});
+
+// ********************
 // Create Pre-Screener
 // ********************
 
 router.post('/create-start', function (req, res) {
 
-  res.redirect('create-study-details');
+    res.redirect('create-study-details');
 
 });
 
