b502a9f4b03ed46d0f066e8b2f8e64066ac00e47
nhsbsa-sthil <sthil@nhsbsa.nhs.uk>
2026-09-14 13:36:12 +0100

    Content updates

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'National Institute for Health and Care Research (NIHR)',
+  serviceName: 'Recruit registry volunteers ',
 
   // Port to run the prototype on locally
   port: 3000
